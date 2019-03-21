1262bd362dccb7a03e17af56d41e6660fb991171
justgo86 <justgo86@yellomobile.com>
2019-03-21 17:48:02 +0900

    라우팅 만들기 전.
    돌아감.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
-
 const http = require('http')
+
+const app = express();
+const router = express.Router();
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -16,10 +17,11 @@ const server = http.createServer((req, res) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/users', require('./api/users'));
-
+app.use('/users', require('./api/users/'))
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
 });
+
+
 
 module.exports = app;
